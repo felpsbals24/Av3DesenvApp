14a3d580a5bff4710dfd46a94a35e266e14220f7
Felpasz <felipe09676@gmail.com>
2026-05-13 12:26:32 -0300

    Conexão feita (Bug descoberto)
    Na questão de quando iniciar em um pc diferente sempre mudar a solução de 4.2.7 pra 4.2.8 ou vice versa, ele só precisa atualizar de alguma forma pra aparecer o design

--- Form1.cs
+++ Form1.cs
@@ -3,23 +3,55 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient; // Ferramenta essencial para o MySQL
 
 namespace Av3DesenvApp
 {
     public partial class Form1 : Form
     {
+        // 1. Variáveis da conexão (Ajustadas e com o MySqlConnection correto)
+        private string strConexao = "Server=localhost;Database=gerenciamento;Uid=root;Pwd=1234;";
+        private MySqlConnection conexao;
+
+        // 2. Método que é rodado assim que a tela é criada
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        // 3. Método responsável por abrir a porta com o Banco de Dados
+        public MySqlConnection Conectar()
         {
+            try
+            {
+                conexao = new MySqlConnection(strConexao);
+                conexao.Open();
+                return conexao;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro ao conectar: " + ex.Message);
+                return null;
+            }
+        }
 
+        // 4. Método responsável por fechar a porta com o Banco de Dados
+        public void Desconectar()
+        {
+            if (conexao != null && conexao.State == ConnectionState.Open)
+            {
+                conexao.Close();
+            }
+        }
+
+        // 5. Espaço reservado para o botão da tela
+        
+            
         }
     }
-}
